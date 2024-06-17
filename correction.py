0c8b9f9749ba7d139001cd80264d995e81fd27ba
joshualee <joshualee032190@gmail.com>
2024-06-17 21:17:04 +0800

    [done] all fix_function

--- correction.py
+++ correction.py
@@ -7,14 +7,186 @@ class IndoSyntaxPostCorrection():
     def get_position(self, zhstr: str, idstr: str):
         pass
     
-    def correction_main(self, zhseg: list, idseg: list) -> list:
+    def correction_main(self, zhseg: list, idseg: list) -> str:
         
-        result: list = []
         # 先修正問題
         # 再修正再問題中的分類
         
         # 修正直述句
+        idseg = self.fix_polite_type(zhseg, idseg)
+        idseg = self.fix_negation_type(zhseg, idseg)
+        idseg = self.fix_state_sentence_type(zhseg, idseg)
+        idseg = self.fix_conjunection_type(zhseg, idseg)
+        idseg = self.fix_predicate_type(zhseg, idseg)
+        idseg = self.fix_question_type(zhseg, idseg)
+        idseg = self.fix_question_case_by_case(zhseg, idseg)
+        idseg = self.fix_time_type(zhseg, idseg)
+        return " ".join(idseg)
         
+    def fix_polite_type(self, zhseg: list, idseg: list) -> list:
+        ref: str = "".join(zhseg)
+        result: str = " ".join(idseg)
+        
+        # 不好意思 ...
+        if "不好意思" == ref[:4]:
+            if "maaf" in result:
+                result = result.replace("maaf", "permisi")
+                
+            if "permisi" not in result:
+                result = "permisi , " + result
+        # 對不起
+        elif "對不起" == ref[:3]:
+            if "maafkan aku" in result or "maafkan saya" in result:
+                result = result.replace("maafkan aku", "maaf")
+                result = result.replace("maafkan saya", "maaf")
+
+            if "maaf" not in result:
+                result = "maaf , " + result
+        # 沒關係
+        elif "沒關係" == ref[:3]:
+            result = result.replace("tidak apa-apa", "gapapa")
+            result = result.replace("tak apa-apa", "gapapa")
+            result = result.replace("tak apa", "gapapa")
+        
+        # 謝謝 不用做
+        
+        return result.split(" ")
+        
+    def fix_negation_type(self, zhseg: list, idseg: list) -> list:
+        ref: str = "".join(zhseg)
+        result: str = " ".join(idseg)
+        # 別 XXX
+        if "別" == ref[0]:
+            if "jangan" not in result:
+                result = "jangan " + result
+        # 不可以 XX
+        elif "不可以" in ref:
+            if "tidak boleh" not in result:
+                result = result.replace("tidak bisa", "tidak boleh")
+                result = result.replace("tak bisa", "tidak boleh")
+        # 禁止 XX
+        elif "禁止" == ref[:2]:
+            if "dilarang" not in result:
+                result = "dilarang " + result
+        # 不
+        elif "不" in ref:
+            result = result.replace("nggak", 'tidak')
+            result = result.replace(" gak ", ' tidak ')
+            result = result.replace(" ga ", ' tidak ')
+        
+        # elif re.search("今天")    
+        return result.split(" ")
+            
+    def fix_state_sentence_type(self, zhseg: list, idseg: list) -> list:
+        ref: str = "".join(zhseg)
+        result: str = " ".join(idseg)
+    
+        # 越來越 XX 
+        if "越來越" in ref:
+            if idseg.count("semakin") == 1 and "lama" not in result:
+                result = result.replace("semakin", "semakin lama semakin")
+        # 有一點 ... 
+        elif "有一點" in ref:
+            if "sesuatu" in result:
+                result = result.replace("sesuatu", "sedikit")
+        # XX 很 XX
+        elif re.search(".*很.*", ref) or re.search(".*蠻.*", ref):
+            if "這" in ref and "sangat" not in result:
+                result = result.replace("ini", "ini sangat", 1)
+            elif "那" in ref and "sangat" not in result:
+                result = result.replace("itu", "itu sangat", 1)
+        # XX 蠻 XX
+        elif re.search(".*蠻.*", ref):
+            if "sangat" in result:
+                result = result.replace("sangat", "terlalu")
+            if "這" in ref and "terlalu" not in result:
+                result = result.replace("ini", "ini terlalu", 1)
+            elif "那" in ref and "terlalu" not in result:
+                result = result.replace("itu", "itu terlalu", 1)
+        # 最 XXX
+        elif re.search("最.*", ref):
+            if "paling" not in result and "yang" in result:
+                result = result.replace("yang", "yang paling")
+        return result.split(" ")
+                
+    def fix_conjunection_type(self, zhseg: list, idseg: list) -> list:
+        ref: str = "".join(zhseg)
+        result: str = " ".join(idseg)
+        
+        # 雖然 A, 但是 B
+        if re.search("雖然.*但是", ref):
+            if "meskipun" in result:
+                result = result.replace("meskipun", "meski")
+            if "meski" not in result:
+                result = "meski " + result
+                
+        # 對 A 來說, B ...
+        elif re.search("對.*來說", ref):
+            # 代表在中間
+            if "bagi" in result and "bagi" != result[:4]:
+                resList = result.split("bagi")
+                result = "bagi" + resList[1].strip() + " , " + resList[0].strip()
+            elif "bagi" not in result:
+                result = "bagi " + result
+                
+        # 因為 A，所以 B
+        elif re.search("因為.*所以", ref):
+            if "karena" not in result:
+                result = "karena " + result
+                
+        # 我以為 A，原來 B
+        elif re.search("以為.*[,|，]原來.*", ref):
+            if "," in result:
+                resList = result.split(",")
+                if "kukira" in resList[0]:
+                    resList[0] = resList[0].replace("kukira", "saya kira")
+                    
+                if "kira" not in resList[0] and "mengira" in resList[0]:
+                    resList[0] = resList[0].replace("mengira", "kira")
+                    
+                if "ternyata" not in resList[1]:
+                    resList[1] = "ternyata " + resList[1]
+                result = resList[0].strip() + " , " + resList[1].strip()
+                result = result.replace("  ", " ")
+        # 越 A 越 B
+        elif re.search("越.*越", ref):
+            if "semakin" not in result:
+                result = "Semakin " + result
+        
+        # 自從 ...
+        elif "自從" == ref[:2]:
+            if "sejak" not in result:
+                result = "sejak " + result
+        
+        # 在 XX 過程中
+        elif re.search("在.*過程中", ref):
+            if "saat" in result and "selama" not in result:
+                result = result.replace("saat", "selama")
+
+            if "selama" not in result:
+                result = "selama " + result
+        # xx, 儘管 xx
+        elif re.search(".*[,|，]儘管.*", ref):
+            if " meski " in result:
+                resList = result.split("meski")
+                result = resList[0].strip() + " meskipun " + resList[1].strip()
+        # 以至於
+        elif "以至於" in ref:
+            if " hingga " in result:
+                result = result.replace(" hingga ", "sehingga")
+        
+        # 要是 XX(原因)，就 XX(結果)
+        elif re.search("要是.*", ref):
+            if "jika" in result:
+                result = result.replace("jika", "kalo")
+                
+            if "kalau" not in result or "kalo" not in result:
+                result = "kalo " + result
+                
+                
+                
+        return result.split(" ")
+    
     def fix_predicate_type(self, zhseg: list, idseg: list) -> list:
         ref: str = "".join(zhseg)
         result: str = " ".join(idseg)
@@ -50,8 +222,6 @@ class IndoSyntaxPostCorrection():
                 
         return result.split(" ")  
 
-        
-        
     def fix_question_type(self, zhseg: list, idseg: list) -> list:
         '''
             # 修正問題 type\n
@@ -137,22 +307,22 @@ class IndoSyntaxPostCorrection():
             if "berapa" in result:
                 if "panjang" not in result:
                     result = result.replace("berapa", "jam berapa")
-                    
+        
         return result.split(" ")
     
-
-    def fix_():
-        pass
+    def fix_time_type(self, zhseg: list, idseg: list) -> list:
         
-        
-        
+        result: str = " ".join(idseg)
+        ref: str = "".join(zhseg)
+        if idseg.count("hari") >= 2:
+            result = result.replace("hari", "", 1)
     
+        return result.split(" ")
         
-
 if __name__ == "__main__":
     correct = IndoSyntaxPostCorrection()
-    zhseg = "他 要 一 個 天花".split()
-    idseg = "dia ingin satu penyakit ku tik".split()
+    zhseg = " 不好意思 ， 今天 星期幾".split()
+    idseg = "segelas air ini sangat besar".split()
     
-    res = correct.fix_predicate_type(zhseg, idseg)
+    res = correct.correction_main(zhseg, idseg)
     print(res)
